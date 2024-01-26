dedc4c1de95e41daffc5043417b5fc837699dfd7
juftin <juftin@juftin.com>
2024-01-25 20:02:25 -0700

    🧪 plugin tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,3 +23,16 @@ def test_main_succeeds(runner: CliRunner) -> None:
 
     result = runner.invoke(cli)
     assert result.exit_code == 0
+
+
+def test_registered_plugins(runner: CliRunner) -> None:
+    """
+    Assert that all registered plugins are available.
+    """
+    from lunchable._cli import cli
+
+    builtin_plugins = ["primelunch", "splitlunch", "pushlunch"]
+
+    for plugin in builtin_plugins:
+        result = runner.invoke(cli, ["plugins", plugin, "--help"])
+        assert result.exit_code == 0
