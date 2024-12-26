f39fd729a87a2bf7194bff13d23396d28d3585e9
Justin Flannery <justin.flannery@juftin.com>
2024-12-26 12:46:16 -0700

    🧹 remove lunchable code

--- lunchable/_config/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Lunch Money Config Namespaces and Helpers
-"""
-
-from .api_config import APIConfig
-from .file_config import FileConfig
-
-__all__ = ["APIConfig", "FileConfig"]
--- lunchable/_version.py
+++
@@ -1,8 +0,0 @@
-"""
-lunchable Version file
-"""
-
-__author__ = "Justin Flannery"
-__email__ = "juftin@juftin.com"
-__application__ = "lunchable"
-__version__ = "1.4.1"
